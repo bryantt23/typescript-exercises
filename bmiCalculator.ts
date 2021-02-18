49a8272a80a913302eaa73c78b033a07328d1003
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-17 21:53:33 -0800

    bmiCalculator using command line arguments

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface BmiValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+    if (args.length < 4) throw new Error("Not enough arguments")
+    if (args.length > 4) throw new Error("Too many arguments")
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3]),
+        }
+    } else {
+        throw new Error("Provided values were not numbers!")
+    }
+}
+
 
 const calculateBmi = (weight: number, height: number): string => {
     const heightCm = height * .01
@@ -13,7 +32,14 @@ const calculateBmi = (weight: number, height: number): string => {
     }
 }
 
+try {
+    const { value1, value2 } = parseArguments(process.argv)
+    console.log(calculateBmi(value1, value2))
+}
+catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+}
 
-console.log(calculateBmi(40, 147))
-console.log(calculateBmi(63, 147))
-console.log(calculateBmi(65, 147))
+// console.log(calculateBmi(40, 147))
+// console.log(calculateBmi(63, 147))
+// console.log(calculateBmi(65, 147))
