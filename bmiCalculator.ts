c849924e71cad9923c57abef755ddd6393544675
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-17 20:51:20 -0800

    Calculate BMI correctly

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,7 +1,19 @@
-const calculateBmi=(weight: number, height:number)=>{
-console.log(weight, height)
-console.log(weight*height)
+
+const calculateBmi = (weight: number, height: number): string => {
+    const heightCm = height * .01
+    const bmi = weight / (heightCm * heightCm)
+    if (bmi < 25) {
+        return "Normal weight"
+    }
+    else if (bmi < 30) {
+        return "Overweight"
+    }
+    else {
+        return "Obese"
+    }
 }
 
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(40, 147))
+console.log(calculateBmi(54, 147))
+console.log(calculateBmi(65, 147))
